e5456acd26e38c9e89e20e3fd45d33c175e0fea4
Samuel Bryner <samuelbryner@gmx.ch>
2020-07-11 16:10:59 +0200

    Clarify cargo flash intro, fix command.
    It's --list-chips not --list-targets. The comment also wasn't very clear and could be misunderstood as "list *connected* targets"

--- snippets/cargo-flash.sh
+++ snippets/cargo-flash.sh
@@ -4,9 +4,9 @@ cargo install cargo-flash
 # In your cargo project directory, call
 cargo flash --release --chip <chip_name>
 
-# Don't know if your target is available
-# and what it's name is?
-cargo flash --list-targets
+# Don't know if your target is supported
+# by cargo flash and what it's name is?
+cargo flash --list-chips
 
 # You can run your examples as usual with
 cargo flash --example <your_example>
