3e1159a571ba56e5964152be87e5f70de3fe852d
Noah Hüsser <noah@huesser.dev>
2023-11-14 01:16:59 +0100

    Fix search

--- src/scripts/components/search.js
+++ src/scripts/components/search.js
@@ -2,12 +2,12 @@ addEventListener(
   "DOMContentLoaded",
   () => {
     docsearch({
-      appId: "ITJWIR3Q7U",
-      apiKey: "12cf716077f5024aa912d2ef899f399b",
-      indexName: "probe-rs",
+      appId: "3ZQ68LINL8",
+      apiKey: "76ecd647479d81cdf7630de9b37c9955",
+      indexName: "probe",
       container: document.getElementById("search"),
       debug: false,
     });
   },
-  false,
+  false
 );
