2c01f6ba24963a4574ea1cdd51e55e8e2ca35bf1
Noah Hüsser <noah@huesser.dev>
2024-05-16 16:36:08 +0100

    Fix page

--- src/_includes/templates/post-details.jsx
+++ src/_includes/templates/post-details.jsx
@@ -1,34 +1,35 @@
-export default (
-  { search, authors, date, readingTime, filters },
-) => {
+export default ({ search, authors, date, readingTime, filters }) => {
   const page = search.page(`type=author author="author"`);
   return (
     <div className="post-details">
       <p>
-        by {authors.map((author, i) => ((
+        by{" "}
+        {authors.map((author, i) => (
           <>
-            {page
-              ? (
-                <a data-pagefind-filter="author" href={page.data.url}>
-                  {author}
-                </a>
-              )
-              : author}
+            {page ? (
+              <a data-pagefind-filter="author" href={page.data.url}>
+                {author}
+              </a>
+            ) : (
+              author
+            )}
             {authors.length == 2
-              ? (i == authors.length - 1 ? "" : " and ")
-              : (authors.length > 2
-                ? (i == authors.length - 1 ? "" : ", ")
-                : "")}
+              ? i == authors.length - 1
+                ? ""
+                : " and "
+              : authors.length > 2
+              ? i == authors.length - 1
+                ? ""
+                : ", "
+              : ""}
           </>
-        )))}
+        ))}
       </p>
       <p>
         <time dateTime={filters.date(date, "DATETIME")}>
           {filters.date(date, "HUMAN_DATE")}
         </time>
       </p>
-
-      <p>{readingTime.minutes} min read</p>
     </div>
   );
 };
