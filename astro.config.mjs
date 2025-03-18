fb9b2a9a88f10cf51b516f1d94fb4a2c5e8efcc2
duck.lol <ducklol22222@gmail.com>
2025-03-18 10:44:26 +0100

    Rewrite with Astro.

---
+++ astro.config.mjs
@@ -0,0 +1,41 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwindcss from '@tailwindcss/vite';
+import icon from "astro-icon";
+
+import expressiveCode from 'astro-expressive-code';
+
+import preact from '@astrojs/preact';
+
+// Update if deploying elsewhere!
+const site = 'https://julianguide.github.io';
+
+// Update this if serving from a different base path.
+// E.g., 'my-repo' for a site at my-git.github.io/my-repo/, or '' for 'probe.rs/'.
+const base = 'probe.rs-astro';
+
+// https://astro.build/config
+export default defineConfig({
+  site,
+  base,
+  outDir: './dist/' + base,
+  integrations: [
+    expressiveCode(),  // Must go before mdx() to add code blocks
+    mdx(),
+    sitemap(),
+    icon({ iconDir: 'src/icons' }),
+    preact(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss(), icon()],
+    server: {
+      // ssh -R 80:localhost:4321 serveo.net
+      allowedHosts: ['.serveo.net'],
+    },
+  },
+
+  compressHTML: true,
+});
--- src/blog.md
+++
@@ -1,6 +0,0 @@
----
-layout: "layouts/blog/list.jsx"
-title: "blog"
-description: "A blog with news and engineering stories about probe-rs"
-order: 20
----
