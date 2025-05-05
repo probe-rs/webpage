62b3689e32de848e8fdf564bc724e42501a888ec
Noah Hüsser <noah@huesser.dev>
2025-05-06 00:44:43 +0200

    Fix title and page url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,30 +1,30 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
-import expressiveCode from 'astro-expressive-code';
+import expressiveCode from "astro-expressive-code";
 
-import preact from '@astrojs/preact';
+import preact from "@astrojs/preact";
 
 // UPDATE these if deploying somewhere new!
-const site = 'https://julianguide.github.io';
+const site = "https://probe.rs";
 
 // UPDATE this if serving from a different base path.
 // E.g., 'my-repo' for a site at my-git.github.io/my-repo/, or '' for 'probe.rs/'.
-const base = '';
+const base = "";
 
 // https://astro.build/config
 export default defineConfig({
   site,
   base,
   integrations: [
-    expressiveCode({ themes: ['vitesse-light', 'vitesse-dark'] }),  // Must go before mdx() to add code blocks
+    expressiveCode({ themes: ["vitesse-light", "vitesse-dark"] }), // Must go before mdx() to add code blocks
     mdx(),
     sitemap(),
-    icon({ iconDir: 'src/icons' }),
+    icon({ iconDir: "src/icons" }),
     preact(),
   ],
 
@@ -32,7 +32,7 @@ export default defineConfig({
     plugins: [tailwindcss(), icon()],
     server: {
       // ssh -R 80:localhost:4321 serveo.net
-      allowedHosts: ['.serveo.net'],
+      allowedHosts: [".serveo.net"],
     },
   },
 
